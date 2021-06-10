c8198665dbc0f1681d4adbb67c7e2c63ab5b4849
Chandan Akiti <chandanakiti@gmail.com>
2021-06-10 00:40:38 -0400

    Update tokenization

--- util/word_encoder.py
+++ util/word_encoder.py
@@ -24,36 +24,57 @@ class BERTWordEncoder(nn.Module):
         word_embeddings = torch.sum(last_four_hidden_states, 0) # [num_sent, number_of_tokens, 768]
         return word_embeddings
     
-    def tokenize(self, raw_tokens):
+    def tokenize(self, raw_tokens, tags):
         raw_tokens = [token.lower() for token in raw_tokens]
-        raw_tokens_list = []
-        # split if too long
-        while len(raw_tokens) > self.max_length - 2:
-            raw_tokens_list.append(raw_tokens[:self.max_length-2])
-            raw_tokens = raw_tokens[self.max_length-2:]
-        if raw_tokens:
-            raw_tokens_list.append(raw_tokens)
-
         indexed_tokens_list = []
+        tag_list = []
         mask_list = []
         text_mask_list = []
-        for raw_tokens in raw_tokens_list:
-            # token -> index
-            tokens = ['[CLS]'] + raw_tokens + ['[SEP]']
-            indexed_tokens = self.tokenizer.convert_tokens_to_ids(tokens)
         
-            # padding
+        curr_split = ['[CLS]']
+        tag_split = []
+        mask_split = np.zeros((self.max_length), dtype=np.int32)
+        text_mask_split = np.zeros((self.max_length), dtype=np.int32)
+        
+        for i, (word, tag) in enumerate(zip(raw_tokens, tags)):
+            tokens = self.tokenizer.tokenize(word)
+            
+            if len(curr_split) + len(tokens) >= self.max_length:
+                indexed_tokens = self.tokenizer.convert_tokens_to_ids(curr_split + ['[SEP]'])
+                while len(indexed_tokens) < self.max_length:
+                    indexed_tokens.append(0)
+                mask_split[:len(indexed_tokens)] = 1
+                
+                indexed_tokens_list.append(indexed_tokens)
+                tag_list.append(tag_split)
+                mask_list.append(mask_split)
+                text_mask_list.append(text_mask_split)
+                
+                curr_split = ['[CLS]']
+                tag_split = []
+                mask_split = np.zeros((self.max_length), dtype=np.int32)
+                text_mask_split = np.zeros((self.max_length), dtype=np.int32)
+            
+            text_mask_split[len(curr_split)] = 1
+            curr_split.extend(tokens)
+            tag_split.append(tag)
+                
+        
+        if tag_split:
+            indexed_tokens = self.tokenizer.convert_tokens_to_ids(curr_split + ['[SEP]'])
             while len(indexed_tokens) < self.max_length:
                 indexed_tokens.append(0)
+            mask_split[:len(indexed_tokens)] = 1
+
             indexed_tokens_list.append(indexed_tokens)
-
-            # mask
-            mask = np.zeros((self.max_length), dtype=np.int32)
-            mask[:len(tokens)] = 1
-            mask_list.append(mask)
-
-            # text mask, also mask [CLS] and [SEP]
-            text_mask = np.zeros((self.max_length), dtype=np.int32)
-            text_mask[1:len(tokens)-1] = 1
-            text_mask_list.append(text_mask)
-        return indexed_tokens_list, mask_list, text_mask_list
+            tag_list.append(tag_split)
+            mask_list.append(mask_split)
+            text_mask_list.append(text_mask_split)
+        
+        # print ("tokens   :", indexed_tokens_list[0])
+        # print ("masks    :", mask_list[0])
+        # print ("text_mask:", text_mask_list[0])
+        # print ("tag_list :", tag_list[0])
+        # print ()
+        
+        return indexed_tokens_list, mask_list, text_mask_list, tag_list
